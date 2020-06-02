355528cb5da86285265235f99e8ae5d9d004d282
FoxHound112263 <camayorquina@unal.edu.co>
2020-06-02 16:17:57 -0500

    reading file

--- code/analysis.R
+++ code/analysis.R
@@ -17,8 +17,54 @@ library(igraph)
 # Network Visualization (D3.js)
 library(networkD3)
 
+# READING
+
 # Set notebook directory.
-MAIN.DIR <- here::here()
+dir <- "C:/Users/User/Desktop/GitHub Projects/TwitterPeaceCol"
 
 # Set file path.
-file.name <- glue('{MAIN.DIR}/static/data/plebiscito.json')
+file.name <- paste0(dir,'/data/plebiscito.json')
+
+# Read each line. 
+tweets.raw.list <- map(
+  .x = read_lines(file = file.name), 
+  .f = rjson::fromJSON
+)
+
+
+# Parse subset of the data into a tibble. 
+tweets.raw.df <- tweets.raw.list %>% 
+  map_df(.f = ~ data.frame(
+    # Select non-user related data.
+    ID = .x$id_str,
+    Created_At = .x$created_at,
+    Text = .x$text, 
+    stringsAsFactors = FALSE
+  )
+  ) %>% 
+  as_tibble()
+
+tweets.raw.df %>% 
+  # We  do not want to display accounts.
+  filter(!str_detect(string = Text, pattern = '@')) %>% 
+  head()
+
+tweets.raw.df %>% glimpse()
+
+tweets.raw.df %>% 
+  slice(1:4) %>% 
+  pull(Created_At) 
+
+
+tweets.raw.df %<>% 
+  mutate(
+    Created_At = Created_At %>% 
+      # Remove zeros.
+      str_remove_all(pattern = '\\+0000') %>%
+      # Parse date.
+      parse_date_time(orders = '%a %b %d %H%M%S %Y')
+  )
+
+tweets.raw.df %>% 
+  filter(!str_detect(string = Text, pattern = '@')) %>% 
+  head()
